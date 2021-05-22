bad6885908f05f12b013b76cb27c2bf5791f0cee
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-05-22 21:48:48 +0400

    added Note model and `has_many notes` relation in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :notes
+
   validates_presence_of :email, :name
   validates :email, uniqueness: true,
     format: { with:  URI::MailTo::EMAIL_REGEXP }
