403da88fdff7419297a396732cf4faf90edfc8e8
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-05-24 14:45:30 +0400

    added models PushNotificationDevice and UserPushNotificationDevice

---
+++ app/models/push_notification_device.rb
@@ -0,0 +1,35 @@
+class PushNotificationDevice < ApplicationRecord
+  has_many :user_push_notification_devices
+  has_many :users, through: :user_push_notification_devices
+
+  enum device_type: %i(android ios)
+
+  validates :device_type, :device_token, presence: true
+
+  class << self
+    # @param devices [Array] Array of devices
+    # @param data [Hash] Data to be passes with the notification
+    def push_notification(devices, data)
+      # ios_device_tokens = get_device_tokens(devices, :ios)
+      android_device_tokens = get_device_tokens(devices, :android)
+
+      if android_device_tokens.present?
+        options = { registration_ids: android_device_tokens, data: data }
+        PushNotify.push(PushNotify::APP_NAME[:android], options)
+      end
+
+      # ios_device_tokens.each do |token|
+      #   options = { device_token: token, data: data, alert: data[:message] }
+      #   PushNotify.push(PushNotify::APP_NAME[:ios], options)
+      # end
+    end
+
+    # Fetch device tokens from device records of the matching device_type
+    # @param devices [Array] Array of devices
+    # @param device_type [Symbol] type of device
+    def get_device_tokens(devices, device_type)
+      devices.select { |d| d.device_type.to_sym.eql?(device_type) }
+        .map(&:device_token).uniq
+    end
+  end
+end
